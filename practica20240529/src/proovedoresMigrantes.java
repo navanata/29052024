bc21220dd94649e1ffb1551f439395fed9132180
Natasha Nicole Navarro Nolasco <nicolenolas24@gmail.com>
2024-05-29 11:39:04 -0600

    practiica29052024

---
+++ practica20240529/src/proovedoresMigrantes.java
@@ -0,0 +1,11 @@
+public class proovedoresMigrantes {
+    public static void main(String[] args) throws Exception {
+
+        String[] proveedoresMigrantes = {"proveedor1", "proveedor2", "proveedor3"};
+
+        System.out.println("Los proveedores migrantes son:");
+        for (String proveedor : proveedoresMigrantes) {
+          System.out.println(proveedor);
+        }
+    }
+}
